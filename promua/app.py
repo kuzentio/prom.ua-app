4860057e92469add8ff904c6167ca25d1c0335de
Igor Kuzmenko <0585ec@gmail.com>
2015-02-04 21:02:22 +0200

    fix db settings

--- promua/app.py
+++ promua/app.py
@@ -1,10 +1,12 @@
+import os
+
 from flask import Flask
 from flask.ext.login import LoginManager
 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///../app.db')
 app.debug = True
 app.secret_key = 'qwerty'
 
