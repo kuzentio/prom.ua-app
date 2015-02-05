345438abec6ff954313325a1db3fd3fe1e876b3d
Igor Kuzmenko <0585ec@gmail.com>
2015-02-05 16:55:06 +0200

    fix templates

--- promua/models.py
+++ promua/models.py
@@ -1,4 +1,5 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 from promua.app import app
 
@@ -11,6 +12,8 @@ class Users(db.Model):
     password = db.Column(db.String(80))
     salt = db.Column(db.String(10))
     email = db.Column(db.String(20), unique=True)
+    Questions = db.relationship('Questions', backref='users')
+    Answers = db.relationship('Answers', backref='users')
 
     def get_id(self):
         return unicode(self.id)
@@ -27,25 +30,26 @@ class Users(db.Model):
 
 class Sessions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     session_id = db.Column(db.String(20), nullable=False, unique=True)
 
 
 class Questions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    who_ask_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete='CASCADE'), nullable=False)
+    who_ask_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     text_question = db.Column(db.String(100))
+    answer = db.relationship("Answers")
 
 
 class Answers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    who_response_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete='CASCADE'), nullable=False)
-    question_id = db.Column(db.Integer, db.ForeignKey("questions.id", ondelete='CASCADE'), nullable=False)
+    who_response_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey("questions.id"), nullable=False)
     text_answer = db.Column(db.String(200))
     votes = db.Column(db.Integer, default=0)
 
 
 class Votes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete='CASCADE'), nullable=False)
-    answer_id = db.Column(db.Integer, db.ForeignKey("answers.id", ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    answer_id = db.Column(db.Integer, db.ForeignKey("answers.id"), nullable=False)
